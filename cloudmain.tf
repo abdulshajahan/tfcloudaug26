030d595b1bffb620673c07b4bb427ab8fed1e1ca
abdulshajahan <117737002+abdulshajahan@users.noreply.github.com>
2026-08-11 21:23:16 +0530

    Parameterize location for virtual network resource

--- cloudmain.tf
+++ cloudmain.tf
@@ -2,10 +2,13 @@ provider "azurerm" {
   # Configuration options
   features {}
 }
+variable rglocation {
+type = string
+}
 resource "azurerm_virtual_network" "hcpvnet" {
   name                = "hcpvnet"
   address_space       = ["10.123.0.0/16"]
-  location            = "centralindia"
+  location            = var.rglocation
   resource_group_name = "TCS-TF-Labs"
   tags = { "env" = "cloud" }
 }
