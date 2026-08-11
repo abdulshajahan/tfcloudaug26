238427dc358a9af7693697d9c30bdb0694e04b99
abdulshajahan <117737002+abdulshajahan@users.noreply.github.com>
2026-08-11 20:30:22 +0530

    Update cloudmain.tf

--- cloudmain.tf
+++ cloudmain.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "hcpvnet" {
   name                = "hcpvnet"
   address_space       = ["10.123.0.0/16"]
-  location            = var.rglocation
-  resource_group_name = var.rgname  
+  location            = "centralindia"
+  resource_group_name = "TCS-TF-Labs"
   tags = { "env" = "cloud" }
 }
